33b7e29aa02afd0be48c1cbc58493e5e35b03a1d
Медведев Андрей <158015192+MedvedAndr@users.noreply.github.com>
2026-09-04 10:14:08 +1000

    Создаст файл библиотеки app-registry.js

---
+++ js/app-registry.js
@@ -0,0 +1,7 @@
+/**
+ * @file Глобальный реестр записей для модулей.
+ * @description Обеспечивает единую точку входа (Singleton) для обмена
+ * данными между изолированными JS-компонентами.
+ * @version 0.1.0
+ * @author Undead Medved {@link https://github.com/MedvedAndr GitHub}
+ */
